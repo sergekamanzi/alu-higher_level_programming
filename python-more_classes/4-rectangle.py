026d6768dd151bd4c26b47e03aadcdae2de43e68
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-07-25 06:06:46 -0700

    4 recta

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -71,26 +71,4 @@ class Rectangle:
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
-# comment here
-
-
-#my_rectangle = Rectangle(2, 4)
-#print(str(my_rectangle))
-#print("--")
-#print(my_rectangle)
-#print("--")
-#print(repr(my_rectangle))
-#print("--")
-#print(hex(id(my_rectangle)))
-#print("--")
-
-# create new instance based on representation
-#new_rectangle = eval(repr(my_rectangle))
-#print(str(new_rectangle))
-#print("--")
-#print(new_rectangle)
-#print("--")
-#print(repr(new_rectangle))
-#print("--")
-#print(hex(id(new_rectangle)))
-#print("--")
+# comment here 
