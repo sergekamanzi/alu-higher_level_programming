6f3b92039287757cfb59cddc9fb2e6ac561d02e8
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-07-26 11:10:03 -0700

    9 recta

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Defines a class Rectangle that inherits from BaseGeometry."""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+BaseGeometry = _import_('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """Represent a rectangle using BaseGeometry."""
 
-    def __init__(self, width, height):
+    def _init_(self, width, height):
         """Intialize a new Rectangle.
 
         :param width: The width of the new Rectangle.
@@ -21,10 +21,10 @@ class Rectangle(BaseGeometry):
 
     def area(self):
         """Return the print() and str() reprentation """
-        return self.__width * self.__height
+        return self._width * self._height
 
-    def __str__(self):
+    def _str_(self):
         """Return a description of the rectangle."""
-        string = "[" + str(self.__class__.__name__) + "] "
-        string += str(self.__width) + "/" + str(self.__height)
+        string = "[" + str(self._class.name_) + "] "
+        string += str(self._width) + "/" + str(self._height)
         return string
