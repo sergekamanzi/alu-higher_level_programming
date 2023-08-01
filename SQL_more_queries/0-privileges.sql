a5184ce690c7ce361d04618d7e7fa4e5f484aeee
KAMANZI-SERGE187 <sergekmz187@gmail.com>
2023-08-01 05:05:56 -0700

    0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,13 @@
--- Lists all privileges of the users user_0d_1 and user_0d_2
+-- Connect to MySQL server
+mysql -u YOUR_USERNAME -p
+
+-- Replace 'YOUR_USERNAME' with your MySQL username, and you will be prompted to enter your password.
+
+-- To list privileges for 'user_0d_1'@'localhost'
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- To list privileges for 'user_0d_2'@'localhost'
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
+-- Exit the MySQL command-line interface
+exit
